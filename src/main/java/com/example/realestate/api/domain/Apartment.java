6fadb34b1162255653f14823ee723965d1b809ca
AniDashyan7 <ani.dashyan@tumo.org>
2023-07-21 20:02:57 +0400

    Apartment class created

---
+++ src/main/java/com/example/realestate/api/domain/Apartment.java
@@ -0,0 +1,34 @@
+package com.example.realestate.api.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Apartment")
+public class Apartment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "rooms")
+    private Integer rooms;
+
+    @Column(name = "floor")
+    private Integer floor;
+
+    @Column(name = "isRenovated")
+    private Boolean isRenovated;
+
+    @Column(name = "furniture")
+    private Boolean furniture;
+
+    @Column(name = "description")
+    private String description;
+}

2cbb7281dfea9a5f3002881f085adb7da22fb549
AniDashyan7 <ani.dashyan@tumo.org>
2023-07-24 18:03:22 +0400

    new

--- src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
+++ src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
@@ -3,17 +3,21 @@ package com.example.realestate.api.repositories;
 import com.example.realestate.api.domain.Apartment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface ApartmentRepository extends JpaRepository<Apartment, Long > {
 
     Optional<Apartment> findApartmentByCity(String city);
     Optional<Apartment>findApartmentByCityAndPrice(String city, Double price);
-
-    Optional <Apartment>findApartmentByCityAndRooms(String city , Integer rooms);
+    Optional <Apartment>findApartmentByCityAndRooms(String city, Integer rooms);
     Optional<Apartment> findApartmentByPrice(Double price);
     Optional<Apartment> findApartmentByRooms(Integer rooms);
-
+    Optional<Apartment> findApartmentByRoomsAndFloor(Integer rooms, Integer floor);
+    Optional<Apartment> findApartmentByRoomsAndDescription(Integer rooms, String description);
     Optional<Apartment> findApartmentByFloor(Integer floor);
+    Optional<Apartment> findApartmentByFloorAndDescription(Integer floor, String description);
     Optional<Apartment> findApartmentByDescription(String description);
+
+    Optional<Apartment> findByAllParameters(String city, Double price, Integer rooms, Integer floor, String description);
 }

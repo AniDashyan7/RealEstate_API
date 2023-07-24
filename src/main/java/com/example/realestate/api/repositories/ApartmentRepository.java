b180f01eec96d9a9db1a7edd9f932e0e903e650e
AniDashyan7 <ani.dashyan@tumo.org>
2023-07-24 19:12:57 +0400

    repo changed

--- src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
+++ src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
@@ -8,16 +8,21 @@ import java.util.Optional;
 
 public interface ApartmentRepository extends JpaRepository<Apartment, Long > {
 
-    Optional<Apartment> findApartmentByCity(String city);
+    Optional<Apartment>findApartmentByCity(String city);
     Optional<Apartment>findApartmentByCityAndPrice(String city, Double price);
     Optional <Apartment>findApartmentByCityAndRooms(String city, Integer rooms);
-    Optional<Apartment> findApartmentByPrice(Double price);
-    Optional<Apartment> findApartmentByRooms(Integer rooms);
-    Optional<Apartment> findApartmentByRoomsAndFloor(Integer rooms, Integer floor);
-    Optional<Apartment> findApartmentByRoomsAndDescription(Integer rooms, String description);
-    Optional<Apartment> findApartmentByFloor(Integer floor);
-    Optional<Apartment> findApartmentByFloorAndDescription(Integer floor, String description);
-    Optional<Apartment> findApartmentByDescription(String description);
+    Optional<Apartment>findApartmentByPrice(Double price);
+    Optional<Apartment>findApartmentByRooms(Integer rooms);
+    Optional<Apartment>findApartmentByRoomsAndFloor(Integer rooms, Integer floor);
+    Optional<Apartment>findApartmentByRoomsAndDescription(Integer rooms, String description);
+    Optional<Apartment>findApartmentByFloor(Integer floor);
+    Optional<Apartment>findApartmentByFloorAndDescription(Integer floor, String description);
+    Optional<Apartment>findApartmentByDescription(String description);
+    Optional<Apartment>findApartmentByCity_PriceAndRooms(String city, Double price, Integer rooms);
+    Optional<Apartment>findApartmentByCity_PriceAndFloor(String city, Double Price, Integer Floor);
+    Optional<Apartment>findApartmentByCity_PriceAndDescription(String City, Double price, String description);
+
+    Optional<Apartment>findApartmentByCity_RoomsAndFloor(String city, Integer rooms, Integer floor);
 
     Optional<Apartment> findByAllParameters(String city, Double price, Integer rooms, Integer floor, String description);
 }

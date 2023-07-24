b778b2323d8f8a92ac8a9e675a7fb51ad4eb6a74
AniDashyan7 <ani.dashyan@tumo.org>
2023-07-24 19:27:34 +0400

    Repository

--- src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
+++ src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
@@ -1,12 +1,13 @@
 package com.example.realestate.api.repositories;
 
 import com.example.realestate.api.domain.Apartment;
+import org.springframework.aop.target.AbstractPrototypeBasedTargetSource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
 
-public interface ApartmentRepository extends JpaRepository<Apartment, Long > {
+public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
 
     Optional<Apartment>findApartmentByCity(String city);
     Optional<Apartment>findApartmentByCityAndPrice(String city, Double price);
@@ -21,8 +22,10 @@ public interface ApartmentRepository extends JpaRepository<Apartment, Long > {
     Optional<Apartment>findApartmentByCity_PriceAndRooms(String city, Double price, Integer rooms);
     Optional<Apartment>findApartmentByCity_PriceAndFloor(String city, Double Price, Integer Floor);
     Optional<Apartment>findApartmentByCity_PriceAndDescription(String City, Double price, String description);
-
     Optional<Apartment>findApartmentByCity_RoomsAndFloor(String city, Integer rooms, Integer floor);
-
-    Optional<Apartment> findByAllParameters(String city, Double price, Integer rooms, Integer floor, String description);
+    Optional<Apartment>findByAllParameters(String city, Double price, Integer rooms, Integer floor, String description);
+    Optional<Apartment>findApartmentByCity_RoomsAnAndDescription(String City, Integer rooms, String description);
+    Optional<Apartment>findApartmentByCity_FloorAndDescription(String city, Integer floor, String Description);
+    Optional<Apartment>findApartmentByPrice_FloorAndDescription(Double price, Integer floor, String description);
+    Optional<Apartment>findApartmentByRooms_FloorAndDescription(Integer rooms, Integer floor, String description);
 }

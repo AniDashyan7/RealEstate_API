8b3826fea02358e058518650a45b6d601f62bd64
davttam <davtyan.tamara6@yahoo.com>
2023-07-24 18:01:25 +0400

    fghbrt

--- src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
+++ src/main/java/com/example/realestate/api/repositories/ApartmentRepository.java
@@ -11,7 +11,16 @@ public interface ApartmentRepository extends JpaRepository<Apartment, Long > {
     Optional<Apartment>findApartmentByCityAndPrice(String city, Double price);
 
     Optional <Apartment>findApartmentByCityAndRooms(String city , Integer rooms);
+
+    Optional <Apartment>findApartmentByCityAndFloor(String city , Integer floor);
+
+    Optional <Apartment>findApartmentByCityAndDescription(String city , String description);
+
     Optional<Apartment> findApartmentByPrice(Double price);
+    Optional<Apartment> findApartmentByPriceAndRooms (Double price, Integer rooms);
+    Optional<Apartment> findApartmentByPriceAndFloor(Double price, Integer floor);
+    Optional<Apartment> findApartmentByPriceAndDescription(Double price, String description);
+
     Optional<Apartment> findApartmentByRooms(Integer rooms);
 
     Optional<Apartment> findApartmentByFloor(Integer floor);
